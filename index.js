cc8010ba04d26314bd92d464d2d39c78e57b1c77
IamLikhon <likhonadnan55@gmail.com>
2023-05-01 21:27:57 +0600

    added the json file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express =require('express');
 const app=express();
+const cors=require('cors')
 const port=process.env.PORT||5000;
+const chefData= require('./data/chefData.json')
 
+app.use(cors())
 
 app.get('/',(req, res)=>{
     res.send('Chef Server side running')
 
+})
+app.get('/allData',(req,res)=>{
+    res.send(chefData)
 })
 
 app.listen(port,()=>{
